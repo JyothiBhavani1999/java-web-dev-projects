a3688b01b5e692917fbde909ec8936fc7c0e0690
JyothiBhavani1999 <bhavanijyothi182@gmail.com>
2024-11-21 19:01:50 -0600

    commit -m "Save Array Practice "

--- control-flow-and-collections/collections-exercises/ArrayPractice.java
+++ control-flow-and-collections/collections-exercises/ArrayPractice.java
@@ -1,19 +1,47 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ArrayPractice {
     public static void main(String[] args) {
-        int[] array = {1,2,3,5,8};
-        for(int i: array)
-        {
-            if(i%2 == 1)
-            {
-                System.out.println("odd numbers are: "+i);
+        int[] array = {1, 2, 3, 5, 8};
+        for (int i : array) {
+            if (i % 2 == 1) {
+                System.out.println("odd numbers are: " + i);
             }
         }
 
         String sample = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
-        String[] stringArray = sample.split("\\.");
-        System.out.println(Arrays.toString(stringArray));
+        String[] stringArray = sample.replaceAll(",", "").replaceAll("\\.", "").split(" ");
 
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i <= 10; i++) {
+            list.add(i + 1);
+        }
+
+        System.out.println("Sum of Even numbers of list: " + EvenSum(list));
+
+
+        ArrayList<String> stringList = new ArrayList<>(Arrays.asList(stringArray));
+        printFiveLetterWords(stringList);
+
+    }
+
+    static int EvenSum(ArrayList<Integer> arrayList) {
+        int sum = 0;
+        for (int i : arrayList) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    static void printFiveLetterWords(ArrayList<String> list){
+        for(String i: list)
+        {
+            if(i.length() == 5){
+                System.out.println("Five leter words: "+ i);
+            }
+        }
     }
 }
